8d526905bc021ee8f00b87ac5a15965ad5133a71
Daniil Kharitonov <84292914+Cleonia21@users.noreply.github.com>
2025-01-20 20:50:54 +0300

    Update home.js

--- js/home.js
+++ js/home.js
@@ -51,7 +51,7 @@ addEventListener('mouseup', function (event) {
 addEventListener('keydown', function(event) {
     if (event.code === 'Enter') { //enterID
         stageSwitch()
-    } else if (event.code === 'Space' || enent.code === 'KeyA') {
+    } else if (event.code === 'Space' || event.code === 'KeyA') {
         result.saveStep(circle.getResult())
         result.alert()
         circle = new Circle()
